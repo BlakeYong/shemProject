97f5edc49c33e421cbf32e5d7ab6cb481e349a8b
eunjae <swejyong@naver.com>
2020-06-01 23:15:01 +0900

    라우터 추가, 미들웨어 작업

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import datetime
 from fastapi import FastAPI
 from routers import airouter
+from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import Response
 from starlette.status import HTTP_200_OK
 from pydantic import BaseModel
+from routers import airouter, APIRouter
 from src import manageUser
 
 app = FastAPI(openapi_url="/api/v1/openapi.json", docs_url="/shemdocs", redoc_url=None)
 app.include_router(airouter.router)
+app.include_router(APIRouter.router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def getRoot(response: Response):

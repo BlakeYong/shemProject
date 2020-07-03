c5d01e0a48eefac08b75b8da2be1bef6491cd584
seojinsarejj <s19009@naver.com>
2020-07-03 21:43:55 +0900

    [CREATE] busapi nearbyStation

--- shem_configs.py
+++ shem_configs.py
@@ -8,5 +8,7 @@ shem_configs = {
     'prod_bucket_name':'',
     'aws_access_key_id':'AKIATGH4KM5RTAUGB65M',
     'aws_secret_access_key':'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',
-    'webhook':'https://discordapp.com/api/webhooks/719874016235487274/75QVLpWN7fcS2tnCu3EcAFB7I5G_vIT67whp8afUqsu7tHTKH0G1fN5Nrv90aZehJlJC'
+    'webhook':'https://discordapp.com/api/webhooks/719874016235487274/75QVLpWN7fcS2tnCu3EcAFB7I5G_vIT67whp8afUqsu7tHTKH0G1fN5Nrv90aZehJlJC',
+    'bus_key':'%2FH3mFys0bo3w3iDd4UtBJmf%2FdE0QtSxa4aa5mBLDuYvx463g9%2BvS6LAw7L1cQnoUz2NPci4D29NXcMHUQb%2FeOw%3D%3D',
+    'bus_url':'http://openapi.gbis.go.kr/ws/rest/busstationservice/searcharound'
 }
